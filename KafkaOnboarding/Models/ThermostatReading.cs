e0fcc63da36ec9770daa5cd0ffe9882b6a4e706a
Fábio - NTIT Solutions <ntitsolutions01@gmail.com>
2026-05-24 06:52:12 -0300

    Initial commit

---
+++ KafkaOnboarding/Models/ThermostatReading.cs
@@ -0,0 +1,9 @@
+﻿// Models/ThermostatReading.cs
+namespace KafkaOnboarding.Models;
+
+public record ThermostatReading(
+    int    SensorId,
+    string Location,
+    double Temperature,
+    DateTime EventTime
+);
---
+++ README.md
@@ -0,0 +1,1 @@
+# kafka-onboarding-dotnet
